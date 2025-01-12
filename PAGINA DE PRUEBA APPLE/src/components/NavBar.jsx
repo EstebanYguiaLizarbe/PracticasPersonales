a9844f778333b333cd456eadb3f787ab81718837
Esteban Yguia <estebanyguia16@gmail.com>
2025-01-11 23:49:33 -0500

    avance

--- PAGINA DE PRUEBA APPLE/src/components/NavBar.jsx
+++ PAGINA DE PRUEBA APPLE/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { appleImg, bagImg, searchImg } from '../utils'
 import { navLists } from '../constans'
 
+
 export const NavBar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px-5 flex justify-between  items-center' >
